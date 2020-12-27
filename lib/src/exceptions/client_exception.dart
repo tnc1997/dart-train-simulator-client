d71506c39d603d7490096a740cec81b0978a2447
Thomas Clark <tnc1997@virginmedia.com>
2020-12-27 06:38:44 +0000

    feat: add client exception

---
+++ lib/src/exceptions/client_exception.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+class ClientException implements Exception {
+  final String message;
+
+  ClientException({
+    this.message,
+  });
+
+  @override
+  String toString() =>
+      message != null ? 'ClientException: $message' : 'ClientException';
+
+  static ProcessResult checkIsSuccessExitCode(ProcessResult result) {
+    if (result.exitCode != 0) {
+      throw ClientException(message: result.stderr);
+    } else {
+      return result;
+    }
+  }
+}
