9c7891c5acb36e58cc3583e43feede2bf691eb91
Thomas Clark <tnc1997@virginmedia.com>
2020-12-27 06:46:35 +0000

    feat: add directory name constants

---
+++ lib/src/constants/directory_name_constants.dart
@@ -0,0 +1,5 @@
+const assetsDirectoryName = 'Assets';
+const contentDirectoryName = 'Content';
+const pluginsDirectoryName = 'plugins';
+const routesDirectoryName = 'Routes';
+const scenariosDirectoryName = 'Scenarios';
