013e6dc982ca04ebd3cb2fdd0952100e66f2af05
Thomas Clark <tnc1997@virginmedia.com>
2020-12-27 06:46:46 +0000

    feat: add file name constants

--- lib/src/constants/file_name_constants.dart
+++ lib/src/constants/file_name_constants.dart
@@ -1,2 +1,5 @@
+const railDriverFileName = 'RailDriver.dll';
 const routePropertiesFileName = 'RouteProperties.xml';
+const scenarioFileName = 'Scenario.xml';
 const scenarioPropertiesFileName = 'ScenarioProperties.xml';
+const serzFileName = 'serz.exe';
